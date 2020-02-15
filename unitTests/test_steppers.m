964bc437b0fde695cc049492b662f817039ad244
Joao Couto <jpcouto@gmail.com>
2020-02-15 17:06:46 -0500

    Added teensy code for the LEDs and unit tests for when setting up the rigs.

---
+++ unitTests/test_steppers.m
@@ -0,0 +1,14 @@
+% connect to bpod
+Bpod('COM13')
+%%
+global BpodSystem
+BpodSystem.StartModuleRelay('TouchShaker1');
+%%
+% lever steppers:
+cVal = '0'; teensyWrite([72 length(cVal) cVal]);
+%%
+cVal = '100'; teensyWrite([72 length(cVal) cVal]);
+%% 
+teensyWrite([71 1 '0' 1 '0']); % Move spouts to zero position
+%%
+teensyWrite([71 2 '95' 2 '95']); % Move spouts to zero position
