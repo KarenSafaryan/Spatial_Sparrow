1ca33a2ee12ebe630ef4de91c9583a1e190af87e
Joao Couto <jpcouto@gmail.com>
2020-06-27 20:32:27 -0400

    WF files are all in the same folder.

--- SpatialSparrow.m
+++ SpatialSparrow.m
@@ -292,7 +292,7 @@ if BpodSystem.ProtocolSettings.triggerWidefield
             fgetl(udpWF)
             fwrite(udpWF,'softtrigger=0')
             fgetl(udpWF)
-            fwrite(udpWF,['expname=' BpodSystem.ProtocolSettings.SubjectName filesep 'onephoton' filesep bhvFile '_2']); % 2 is the number of widefield channels
+            fwrite(udpWF,['expname=' BpodSystem.ProtocolSettings.SubjectName filesep 'onephoton' filesep bhvFile filesep bhvFile '_2']); % 2 is the number of widefield channels
             fgetl(udpWF)
             disp(' -> WIDEFIELD labcams connected.');
         
